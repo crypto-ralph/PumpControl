14418cb743f4446233c95a26023de1c23cefc116
CryptoRalph <doxter@protonmail.com>
2023-11-19 17:06:39 +0100

    Mocks update

--- test/mocks/pump_mock.py
+++ test/mocks/pump_mock.py
@@ -5,8 +5,8 @@ from test.mocks.temp_mock import TempSensorMock
 class PumpMock:
     def __init__(self, temp_manager_mocked: TempManager):
         self.power = 0
-        if (not isinstance(temp_manager_mocked.sensor1, TempSensorMock)
-                and not isinstance(temp_manager_mocked.sensor2, TempSensorMock)):
+        if (not isinstance(temp_manager_mocked.sensors[0], TempSensorMock)
+                and not isinstance(temp_manager_mocked.sensors[1], TempSensorMock)):
             raise Exception("Temp Manager has to be mocked")
         self.temp_manager = temp_manager_mocked
 
@@ -15,6 +15,6 @@ class PumpMock:
             self.power = value
 
     def update(self):
-        temp = self.temp_manager.sensor2.temperature
-        self.temp_manager.sensor2.temperature = temp + self.power * 0.15
-        self.temp_manager.sensor2.temperature -= 0.13
+        temp = self.temp_manager.sensors[1].temperature
+        self.temp_manager.sensors[1].temperature = temp + self.power * 0.15
+        self.temp_manager.sensors[1].temperature -= 0.13
